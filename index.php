34dadad43b11b313967c039b36309113f3b737f5
AhmadNauval07 <anauval339@gmail.com>
2024-11-21 17:36:26 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$Nama ="Ahmad Nauval";
+$Nim ="312310677";
+$Program ="Teknik informatika";
+
+?>
+
+<h1>
+    Perkenalan Diri Saya
+</h1>
+<p>
+Nama :<?php echo $Nama?>
+<p>
+Nim :<?php echo $Nim?>
+<p> 
+Program :<?php echo $Program?>
+</p>
